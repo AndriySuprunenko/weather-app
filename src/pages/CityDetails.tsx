e7764664dfb5a4ec7a12957d7f2872df43381e83
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2024-12-14 15:29:51 +0200

    add some fixes

--- src/pages/CityDetails.tsx
+++ src/pages/CityDetails.tsx
@@ -3,6 +3,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchWeatherDetails } from '../store/slices/weatherSlice';
 import { RootState } from '../store';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
 
 import style from '../styles/cityDetails.module.scss';
 
@@ -23,26 +32,43 @@ const CityDetails: React.FC = () => {
   if (error) return <p>Помилка: {error}</p>;
   if (!details) return <p>Дані відсутні</p>;
 
+  const data = details.hourlyForecast.map((hour) => ({
+    time: hour.time,
+    temperature: Math.round(hour.temperature),
+  }));
+
   return (
     <div className={style.wrapper}>
       <h2 className={style.name}>{details.city}</h2>
       <h3 className={style.title}>Hourly Forecast</h3>
-      <table className={style.table}>
-        <thead className={style.thead}>
-          <tr className={style.tr}>
-            <th className={style.th}>Час</th>
-            <th className={style.th}>Температура (°C)</th>
-          </tr>
-        </thead>
-        <tbody className={style.tbody}>
-          {details.hourlyForecast.map((hour, index) => (
-            <tr className={style.tr} key={index}>
-              <td className={style.td}>{hour.time}</td>
-              <td className={style.td}>{Math.round(hour.temperature)}°C</td>
+      <div className={style.container}>
+        <table className={style.table}>
+          <thead className={style.thead}>
+            <tr className={style.tr}>
+              <th className={style.th}>Час</th>
+              <th className={style.th}>Температура (°C)</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className={style.tbody}>
+            {details.hourlyForecast.map((hour, index) => (
+              <tr className={style.tr} key={index}>
+                <td className={style.td}>{hour.time}</td>
+                <td className={style.td}>{Math.round(hour.temperature)}°C</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <ResponsiveContainer width='100%' height={300} className={style.graf}>
+          <LineChart data={data}>
+            <CartesianGrid stroke='#ccc' strokeDasharray='5 5' />
+            <XAxis dataKey='time' />
+            <YAxis />
+            <Tooltip />
+            <Line type='monotone' dataKey='temperature' stroke='#8884d8' />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
